9e6faf587341dab7a1940dde07df3e916312e477
Zhd68 <zhdanovalexey25@gmail.com>
2022-01-06 10:37:28 +0300

    Added Fibonacci Sequence

---
+++ interesting-tasks/fibonacci/fibonacci_for.py
@@ -0,0 +1,7 @@
+fibonachi_list1 = [0, 1]
+
+for num in range(2,10):
+    fibonachi_list1.append(fibonachi_list1[num - 1] + fibonachi_list1[num - 2])
+
+#fibonachi_list1.sort(reverse = True)
+print(fibonachi_list1)
